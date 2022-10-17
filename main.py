7c7da603c9c923ca3ad9c213cb1973de98278c69
icepick4 <remi.jara@laposte.net>
2022-10-17 09:53:27 +0200

    first init

---
+++ main.py
@@ -0,0 +1,14 @@
+"""main file"""
+import tweepy
+
+import config
+
+client = tweepy.Client(access_token=config.ACCESS_TOKEN,
+                       access_token_secret=config.ACCESS_TOKEN_SECRET,
+                       consumer_key=config.API_KEY,
+                       consumer_secret=config.API_KEY_SECRET
+                       )
+# Replace the text with whatever you want to Tweet about
+response = client.create_tweet(text='hello world')
+
+print(response)
