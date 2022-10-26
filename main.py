8e3b8b91516b3c3977d186afe0324fdf2b349843
icepick4 <remi.jara@laposte.net>
2022-10-26 13:29:25 +0200

    adding the script to choose manual or automatic tweet (for the server)

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 """main file"""
-from tweet import init_tweet_trending
+import time
+
+from tweet import init_tweet_top, init_tweet_trending
 
 
-def main():
+def main(mannual):
     """main function"""
-    # tweet the trending games
-    init_tweet_trending(False)
-    # tweet the top games
-    # init_tweet_top(False)
+    if mannual:
+        # tweet the trending games
+        init_tweet_trending(False)
+        # tweet the top games
+        time.sleep(60)
+        init_tweet_top(False)
+    else:
+        while True:
+            # tweet every 2 hours
+            init_tweet_trending(False)
+            time.sleep(60 * 5)
+            init_tweet_top(False)
+            time.sleep(10800)
 
 
 if __name__ == '__main__':
-    main()
+    OPTION = ''
+    while OPTION not in ['y', 'n']:
+        OPTION = input('Do you want to tweet manually (y/n)? ')
+    if OPTION == 'y':
+        main(True)
+    else:
+        main(False)
